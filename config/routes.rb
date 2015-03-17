7b9d30f36faead84265ecee88c92d10f24def125
JohnMorales <jmorales@gmail.com>
2015-03-17 08:31:56 -0400

    Adding logic from andrewmp1/spree_api_extend as starting point

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  # Add your extension routes here
+  namespace :api, :defaults => { :format => 'json' } do
+    resource :session, controller: :user_session
+  end
 end
